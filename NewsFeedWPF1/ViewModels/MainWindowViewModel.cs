64f43255a601bff9f3b2065b4d72822e09c66b95
DeZ4526 <karpov.k-r@yandex.ru>
2023-04-11 21:45:05 +0300

    Create ViewModel and the best UI

---
+++ NewsFeedWPF1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using SNewsLoader;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace NewsFeedWPF1.ViewModels
+{
+	class MainWindowViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName] string prop = "")
+		=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+
+		private News[] newsArray;
+		public News[] News
+		{
+			get
+			{
+				return newsArray;
+			}
+			set
+			{
+				newsArray = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+		public ICommand Reload
+		{
+			get => new DelegateCommand((obj) =>
+			{
+				News = NewsGetter.GetAllNews();
+			});
+		}
+	}
+}
